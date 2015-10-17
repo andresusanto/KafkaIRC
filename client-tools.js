2f46a3475c9e0002621b7a6d88f5f9f26640422e
andresusanto <andre@andresusanto.info>
2015-10-17 23:09:01 +0700

    Import old project (rabbit mq based)

---
+++ client-tools.js
@@ -0,0 +1,73 @@
+var amqp = require('amqplib/callback_api');
+
+module.exports = {
+	serverAddress : '167.205.32.46',
+	queueIdentifier : '13512028_',
+	serverIdentifier : '13512028_server_',
+	nick : '',
+	messages : [],
+	
+	genToken : function(){
+		return Math.random().toString() + Math.random().toString() + Math.random().toString();
+	},
+	
+	reg : function (nick){
+		var _acc = this;
+		var req = {};
+		req.request = 'nick';
+		req.command = nick;
+		
+		this.queryServer(req, function(result){
+			if (result == "1"){
+				console.log("Nick Changed to " + nick);
+				_acc.nick = nick;
+				
+				_acc.listen();
+			}else{
+				console.log("Nick is used by another person! Please select another!");
+			}
+		});
+	},
+	
+	queryServer : function (query, callback){
+		var serverIdentifier = this.serverIdentifier;
+		var token = this.genToken();
+		
+		query.nick = this.nick;
+		amqp.connect('amqp://' + this.serverAddress, function(err, conn) {
+			conn.createChannel(function(err, ch) {
+				ch.assertQueue('', {exclusive: true}, function(err, q) {
+					var corr = token;
+
+					ch.consume(q.queue, function(msg) {
+						if (msg.properties.correlationId == corr) {
+							callback(msg.content.toString());
+							setTimeout(function() { conn.close() }, 500);
+						}
+					}, {noAck: true});
+					
+					var query_string = JSON.stringify(query);
+					ch.sendToQueue(serverIdentifier, new Buffer(query_string), { correlationId: corr, replyTo: q.queue });
+				});
+			});
+		});
+	},
+	
+	listen : function(){
+		var nick = this.nick;
+		var queueIdentifier = this.queueIdentifier;
+		var messages = this.messages;
+		
+		amqp.connect('amqp://' + this.serverAddress, function(err, conn) {
+			conn.createChannel(function(err, ch) {
+				var q = queueIdentifier + nick;
+
+				ch.assertQueue(q, {durable: false});
+				ch.consume(q, function(msg) {
+					messages.push(msg.content.toString());
+				}, {noAck: true});
+			});
+		});
+	}
+	
+};
---
+++ server-tools.js
@@ -0,0 +1,100 @@
+var amqp = require('amqplib/callback_api');
+
+module.exports = {
+	serverAddress : '167.205.32.46',
+	queueIdentifier : '13512028_',
+	channelIdentifier : '13512028_channel_',
+	serverIdentifier : '13512028_server_',
+	clients : [],
+		
+	send : function(queueName, text){
+			var queueIdentifier = this.queueIdentifier;
+			amqp.connect('amqp://' + this.serverAddress, function(err, conn) {
+				conn.createChannel(function(err, ch) {
+					var q = queueIdentifier + queueName;
+
+					ch.assertQueue(q, {durable: false});
+					ch.sendToQueue(q, new Buffer(text));
+					//console.log(" [x] Sent 'Hello World!'" + q);
+				});
+			});	
+	},
+	
+	listen : function(){
+		clients = this.clients;
+		serverIdentifier = this.serverIdentifier;
+		channelIdentifier = this.channelIdentifier;
+		queueIdentifier = this.queueIdentifier;
+		
+		amqp.connect('amqp://' + this.serverAddress, function(err, conn) {
+			conn.createChannel(function(err, ch) {
+				var q = serverIdentifier;
+
+				ch.assertQueue(q, {durable: false});
+				ch.prefetch(1);
+				ch.consume(q, function (msg) {
+					var answer = "0";
+					var req = JSON.parse(msg.content.toString());
+					
+					switch (req.request){
+						case 'nick':
+							if (clients[req.command] == undefined){
+								answer = "1";
+								clients[req.command] = [];
+							}else{
+								answer = "0";
+							}
+							break;
+						
+						case 'join':
+							if (clients[req.nick].indexOf(req.command) == -1){
+								ch.assertExchange(channelIdentifier + req.command, 'fanout', {durable: false});
+								ch.bindQueue(queueIdentifier + req.nick, channelIdentifier + req.command, '');
+								clients[req.nick].push(req.command);
+								answer = "1";
+							}else{
+								answer = "0";
+							}
+							break;
+						
+						case 'leave':
+							if (clients[req.nick].indexOf(req.command) != -1){
+								ch.assertExchange(channelIdentifier + req.command, 'fanout', {durable: false});
+								ch.unbindQueue(queueIdentifier + req.nick, channelIdentifier + req.command, '');
+								
+								clients[req.nick].splice(clients[req.nick].indexOf(req.command));
+								answer = "1";
+							}else{
+								answer = "0";
+							}
+							break;
+							
+						case 'sendto':
+							if (clients[req.nick].indexOf(req.to) != -1){
+								ch.publish(channelIdentifier + req.to, '', new Buffer('[' + req.to + ']\t' + req.nick + ': ' + req.message));
+								answer = "1";
+							}else{
+								answer = "0";
+							}
+							break;
+						
+						case 'sendall':
+							if (clients[req.nick] != undefined){
+								clients[req.nick].forEach(function(element){
+									ch.publish(channelIdentifier + element, '', new Buffer('[' + element + ']\t' + req.nick + ': ' + req.message));
+								});
+								answer = "1";
+							}else{
+								answer = "0";
+							}
+							break;
+						
+					}
+					
+					ch.sendToQueue(msg.properties.replyTo, new Buffer(answer), {correlationId: msg.properties.correlationId});
+					ch.ack(msg);
+				});
+			});
+		});
+	}
+};
